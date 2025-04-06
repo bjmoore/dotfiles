10c328621db459b7d2a88eb0cea231d4454a08b9
Benjamin Moore <benjaminjm@gmail.com>
2025-04-06 16:51:56 -0700

    Add lsp-mode

--- init.el
+++ init.el
@@ -4,13 +4,21 @@
 (package-initialize)
 (package-refresh-contents)
 
-(require 'rust-mode)
+(use-package evil)
+(use-package rust-mode)
+(use-package lsp-mode
+  :ensure t
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :config
+  (add-hook 'rust-mode-hook 'lsp))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(evil gnu-elpa-keyring-update rust-mode)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
