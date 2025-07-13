e74651efc71bbf7c21e7ce625ad39de6d67765b0
Benjamin Moore <benjaminjm@gmail.com>
2025-07-13 11:32:41 -0700

    add flycheck-rust

--- init.el
+++ init.el
@@ -4,12 +4,20 @@
 (package-initialize)
 (package-refresh-contents)
 
-(use-package evil)
+(use-package evil
+  :ensure t)
+(use-package flycheck
+  :ensure t)
+(use-package flycheck-rust
+  :ensure t)
+
+(with-eval-after-load 'rust-ts-mode
+  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+
 (use-package lsp-mode
   :ensure t
   :init
   (setq lsp-keymap-prefix "C-c l")
-  (setq lsp-diagnostics-provider :none)
   :config
   (add-hook 'rust-ts-mode-hook 'lsp-deferred)
   (add-hook 'typescript-ts-mode-hook 'lsp-deferred))
@@ -28,7 +36,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(evil flycheck flycheck-rust gnu-elpa-keyring-update lsp-mode
+	  rust-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
